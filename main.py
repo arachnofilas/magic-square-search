3ae32c3f430b84847fdcf267c41828704e5bbd4b
ar4ch0 <d.mikalkenas@gmail.com>
2017-04-06 08:30:51 +0300

    Adds square matrix extractor and largest magic square dimension finder

--- main.py
+++ main.py
@@ -27,13 +27,42 @@ def parser():
 
 def validate(rows, columns):
     if rows < 2 or columns < 2:
-        print('Magic number cannot appear in a matrix which is ' +
+        print('Magic square cannot appear in a matrix which is ' +
         'smaller than [2,2]')
         sys.exit(1)
-
+        '''
+        fixed type "magic number" to "magic square"
+        '''
 
 def generate_matrix(rows, columns):
     return np.random.randint(10, size=(rows, columns))
+
+
+def get_square_matrix_from(matrix):
+    rows = matrix.shape[0]
+    columns = matrix.shape[1]
+    #print('Rows: ' + str(rows) + '; Columns: ' + str(columns))
+    max_square_dimension = get_max_square_dimension(matrix)
+    #print('Max square dimension is: ' + str(max_square_dimension))
+    i = 1
+    for square_dimension in range(2,max_square_dimension+1):
+        for row in range(rows):
+            for column in range(columns):
+                if row < rows - i and column < columns - i:
+                    temp = matrix[row:row+square_dimension,column:column+square_dimension]
+                    print(temp)
+                else:
+                    break
+        i += 1
+
+
+def get_max_square_dimension(matrix):
+    rows = matrix.shape[0]
+    columns = matrix.shape[1]
+    max_dimension = rows
+    if columns < rows:
+        max_dimension = columns
+    return max_dimension
 
 
 def main():
@@ -41,6 +70,10 @@ def main():
     validate(args.rows, args.columns)
     matrix = generate_matrix(args.rows, args.columns)
     print(matrix)
+    get_square_matrix_from(matrix)
+    '''
+    jei sukurta matrica iskart square tai pirma mest i is_magic_square, tik po to i get_square_matrix_from
+    '''
 
 
 if __name__ == '__main__':
