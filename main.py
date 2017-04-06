703d00aa16048a37edbbf0e969b68c44c77ba2d4
ar4ch0 <d.mikalkenas@gmail.com>
2017-04-06 17:53:43 +0300

    Fixes style according to flake8

--- main.py
+++ main.py
@@ -2,14 +2,13 @@
 
 import argparse
 import sys
-import os
 import numpy as np
 
 
 def parser():
     parser = argparse.ArgumentParser(
         description='This tool searches for a number of magic squares ' +
-            'in a given [n,m] matrix.')
+                    'in a given [n,m] matrix.')
     parser.add_argument(
         'rows',
         metavar='rows',
@@ -28,7 +27,7 @@ def parser():
 def validate(rows, columns):
     if rows < 2 or columns < 2:
         print('Magic square cannot appear in a matrix which is ' +
-            'smaller than [2,2]')
+              'smaller than [2,2]')
         sys.exit(1)
 
 
@@ -36,17 +35,17 @@ def generate_matrix(rows, columns):
     return np.random.randint(10, size=(rows, columns))
 
 
-def get_square_matrix_from(matrix,magic_square_counter):
+def get_square_matrix_from(matrix, magic_square_counter):
     rows = matrix.shape[0]
     columns = matrix.shape[1]
     max_square_dimension = get_max_square_dimension(matrix)
     i = 1
-    for square_dimension in range(2,max_square_dimension+1):
+    for square_dimension in range(2, max_square_dimension+1):
         for row in range(rows):
             for column in range(columns):
                 if row < rows - i and column < columns - i:
                     temp_matrix = matrix[row:row+square_dimension,
-                        column:column+square_dimension]
+                                         column:column+square_dimension]
                     if is_magic_square(temp_matrix):
                         magic_square_counter += 1
                         if magic_square_counter > 0:
@@ -77,12 +76,12 @@ def is_magic_square(square_matrix):
     temp_sum = 0
     for row in range(rows):
         for column in range(columns):
-            temp_sum += square_matrix[row,column]
+            temp_sum += square_matrix[row, column]
         magic_sum.append(temp_sum)
         temp_sum = 0
     for column in range(columns):
         for row in range(rows):
-            temp_sum += square_matrix[row,column]
+            temp_sum += square_matrix[row, column]
         magic_sum.append(temp_sum)
         temp_sum = 0
     for argument in range(first_diagonal.shape[0]):
@@ -100,14 +99,15 @@ def main():
     args = parser()
     validate(args.rows, args.columns)
     matrix = generate_matrix(args.rows, args.columns)
-    print('\nGenerated matrix: \n\n'+ str(matrix) + '\n')
+    print('\nGenerated matrix: \n\n' + str(matrix) + '\n')
     print('Magic squares:\n')
     magic_square_counter = 0
-    get_square_matrix_from(matrix,magic_square_counter)
+    get_square_matrix_from(matrix, magic_square_counter)
 
-    #my_matrix = np.array([[4, 9, 2, 3, 5], [3, 5, 7, 4, 2], [8, 1, 6, 6, 2], [1, 1, 6, 6, 2]])
-    #print('\nMy matrix: \n\n'+ str(my_matrix) + '\n')
-    #get_square_matrix_from(my_matrix,magic_square_counter)
+    # my_matrix = np.array([[4, 9, 2, 3, 5], [3, 5, 7, 4, 2],
+    #                       [8, 1, 6, 6, 2], [1, 1, 6, 6, 2]])
+    # print('\nMy matrix: \n\n' + str(my_matrix) + '\n')
+    # get_square_matrix_from(my_matrix, magic_square_counter)
 
 
 if __name__ == '__main__':
